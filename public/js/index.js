09eebc8e4c533c0fdd26762fc2da3cf93c60ecb7
kamal01233210 <sehrawatkamal@gmail.com>
2018-05-26 22:31:44 +0530

    add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+var socketIo = io();
+socketIo.on('connect',function (){
+	console.log('connected');
+
+	/*socketIo.emit('createEmail',{
+		to:'jen@example.com',
+		text:'this is andrew'
+	});*/
+
+	socketIo.emit('createMessage',{
+		from:"kamal",
+		text:"this works for me"
+	});
+});
+socketIo.on('disconnect',function (){
+	console.log('disconnected');
+});
+
+/*socketIo.on('newEmail',function(email){
+	console.log('new email',email);
+});*/
+
+socketIo.on('newMessage',function(newMessage){
+	console.log('newMessage',newMessage);
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,24 @@ app.use(express.static(publicPath));
 io.on('connection',(socketIo)=>{
 	console.log('New User connected');
 
+	/*socketIo.emit('newEmail',{
+		from:'mike@example.com',
+		text:'Hey,what is going on',
+		createAt : '123'
+	});*/
+
+	socketIo.emit('newMessage',{
+		from:"server",
+		text:"hahah"
+	});
+
+	/*socketIo.on('createEmail',(newEmail)=>{
+		console.log('createEmail',newEmail);
+	});*/
+
+	socketIo.on('createMessage',(message)=>{
+		console.log('createMessage',message);
+	})
 	socketIo.on('disconnect',()=>{
 		console.log('user disconnected');
 	});
