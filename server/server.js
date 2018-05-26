9818b6363d5d14e4d94ca122164e4eca63d56f0f
kamal01233210 <sehrawatkamal@gmail.com>
2018-05-26 21:53:38 +0530

    connect disconnect

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+
+const socket = require('socket.io');
 const publicPath = path.join(__dirname,'../public');
 var app = express();
+
+var server = http.createServer(app)
+var io = socket(server);
+
 app.use(express.static(publicPath));
 
+io.on('connection',(socketIo)=>{
+	console.log('New User connected');
+
+	socketIo.on('disconnect',()=>{
+		console.log('user disconnected');
+	});
+});
 const port = process.env.PORT ||2005;
 
-app.listen(port,function(){
+server.listen(port,function(){
 	console.log('Server Listening on port '+port);
 });
